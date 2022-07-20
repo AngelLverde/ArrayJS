cfd42e3dfec2c9be3c588a0f6ef5ed902a5c7093
Angel-Verde <verdeangel95@hotmail.com>
2022-07-20 20:11:57 -0300

    primer entrega proyecto final

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//-- Ingreso del usuario--//
+//-- Ingreso del usuario--// 
 let nombreIngresado = prompt ("ingresa tu nombre");
 let ingresarMail = prompt ("Bienvenido a Cartagena "+nombreIngresado+" dejanos tu mail para recibir el newsletter");
 
@@ -8,7 +8,12 @@ let ingresarMail = prompt ("Bienvenido a Cartagena "+nombreIngresado+" dejanos t
 
 
 //--------------------------------------------------Creando Array-----------------------------------------------//
-let carrito = [];
+let carrito = [ 
+    {id: 1, nombre: "remeras", marca: "nike", precio: 2000},
+    {id: 2, nombre: "zapatillas", marca: "adidas", precio: 20000},
+    {id: 3, nombre: "buzos", marca: "puma", precio: 5000}
+
+];
 
 function agregarAlCarrito(producto) {
     carrito.push(producto)
@@ -27,24 +32,40 @@ function agregarAlCarrito(producto) {
 
 //  Cuando el usuario agregue el producto al carrito, esta llamando a cada uno de los productos//
 
-agregarAlCarrito({id: 1, nombre: "remeras nike", precio: 2000});
-agregarAlCarrito({id: 2, nombre: "zapatillas adidas", price: 20000});
-agregarAlCarrito({id: 3, nombre: "buzos puma", precio: 5000});
+agregarAlCarrito({id: 1, nombre: "remeras", marca: "nike", precio: 2000});
+agregarAlCarrito({id: 2, nombre: "zapatillas", marca: "adidas", precio: 20000});
+agregarAlCarrito({id: 3, nombre: "buzos", marca: "puma", precio: 5000});
 
 
 //cuando el usuario elimine un producto del carrito, se ejecutara esta funcion//
 
 function borrarProductoDelCarrito (idDelProducto) {
     const index = carrito.findIndex ((producto)=> producto.id === idDelProducto);
-    carrito.splice(index, 1);
-    console.log (carrito);
+    if (index != -1) {
+        carrito.splice(index, 1);
+        console.log (carrito);
+    }else {
+        return "eliminaste el producto del carrito";
+    
+    }
+    
 }
 
 borrarProductoDelCarrito (1);
 borrarProductoDelCarrito (2);
 
+//Se aplica metodo de filtrado sobre array creado, para identificar el producto que cumpla con la condicion solicitada//
 
+let filtroPorPrecio = carrito.filter(filtro);
+function filtro(carrito){
+    if (carrito.precio >= 2000){
+        console.log (carrito)
+        return true
+    } else{
+        return false
+    }
 
+    }
 
 
 
